24eb2bdadbb25995d0d8786ea5321b2e58e8640c
aditya muley <aditya3muley@gmail.com>
2021-06-15 23:31:11 +0530

    added insert methode

--- app/app.go
+++ app/app.go
@@ -28,11 +28,11 @@ func checkError(err error) error {
 	}
 	return nil
 }
+
 func main() {
 	dbInfo := fmt.Sprintf("user=%s password=%d host=%s port=%d sslmode=disable", username, password, host, port)
 	db, err := sql.Open("postgres", dbInfo)
 	checkError(err)
-	fmt.Println(dbInfo)
 	err = resetDB(db, dbName)
 	checkError(err)
 	db.Close()
@@ -41,7 +41,22 @@ func main() {
 	checkError(err)
 	err = createTable(db)
 	checkError(err)
+	numbers := []string{"1234567890", "123 456 7891", "123 456 7892", "123 456-7893", "123-456-7894", "123-456-7890", "1234567892", "123-456-7892"}
+	for _, number := range numbers {
+		_, err := insertRow(db, number)
+		checkError(err)
+	}
 	defer db.Close()
+}
+
+func insertRow(db *sql.DB, num string) (int, error) {
+	var id int
+	statement := "INSERT INTO phone_numbers(number) values($1) RETURNING ID"
+	err := db.QueryRow(statement, num).Scan(&id)
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
 }
 
 func createTable(db *sql.DB) error {
